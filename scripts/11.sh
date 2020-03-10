793bbf0faa66cd1a5e8b88791df903fd83e05cb0
Gerson Itiro Hidaka <itirohidaka@msn.com>
2020-03-10 16:58:11 -0300

    Correcao do script 11

--- scripts/11.sh
+++ scripts/11.sh
@@ -1,4 +1,4 @@
 # /bin/sh
 
 echo "Tarefa 11.01.01 - Implementando o CoreDNS"
-kubectl apply -f https://raw.githubusercontent.com/itirohidaka/kthw_aws_ptbr/master/yaml/coredns.yaml
+kubectl apply -f https://raw.githubusercontent.com/itirohidaka/kmd/master/yaml/coredns.yaml
