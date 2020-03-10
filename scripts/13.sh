37a948da868a4ea1e1e18e1c7cc5a05c9e92d537
Gerson Itiro Hidaka <itirohidaka@msn.com>
2020-03-10 20:54:50 -0300

    Melhorias no script 13

--- scripts/13.sh
+++ scripts/13.sh
@@ -25,7 +25,7 @@ do
    NI=$(aws ec2 describe-network-interfaces --filters "Name=group-name,Values=kubernetes" --query "NetworkInterfaces[].Groups[].GroupName")
    LNI=$(echo $NI | wc -c)
    i=$((LNI))
-   echo "."
+   echo "Aguardando exclusão das NetInterfaces... $LNI"
    sleep 5
 done
 
